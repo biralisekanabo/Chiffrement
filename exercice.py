e882b25bc452fe2bb95aaceade48c749054fe8aa
Blessing print <blessingprint203@gmail.com>
2026-03-30 16:46:57 +0200

    le chiffrement 2

--- exercice.py
+++ exercice.py
@@ -1,11 +1,35 @@
 #Fernet algorithme dechiffrement symetrique
 from cryptography.fernet import Fernet
+import mysql.connector
+import os
 
-message_clair="Bonjour Blessing "
+def connecter():
+bd=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=""
+    database="securite"
+) 
 
-cle_de_chiffrement=Fernet.generate_key()
+return curseur 
+def chiffrer(val_message_clair):
+    cle=os.environ.get("Fernet")
+    chiffrement=Fernet(cle.encode)
+    message_chiffre=chiffrement.encrypt(val_message_clair.encode())
+    return message_chiffre
 
-chiffrement=Fernet(cle_de_chiffrement)
-message_chiffre=chiffrement.encrypt(message_clair.encode())
-print(f"messageclaire:{message_clair}")
-print(f"messagechiffre:{message_chiffre}")
+def enregistrer_utilisateur():
+
+    pass
+
+print(chiffrer("Bonjour tout le monde"))
+#message_clair="Bonjour Blessing "
+
+#cle_de_chiffrement=Fernet.generate_key()
+
+#chiffrement=Fernet(cle_de_chiffrement)
+#message_chiffre=chiffrement.encrypt(message_clair.encode())
+#print(f"message clair:{message_clair}")
+#print(f"messagechiffre:{message_chiffre}")
+#message_dechiffre= chiffrement.decrypt(message_chiffre).decode()
+#print(f"message dechiffre:{message_dechiffre}")
